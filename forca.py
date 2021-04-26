c5b5ab6ef3225b5e8c92726e382c5b8bf805d4b1
CaioCSB <caio.bispo1999@gmail.com>
2021-04-26 12:32:58 -0300

    implementação na forca

--- forca.py
+++ forca.py
@@ -1,35 +1,53 @@
+import random
 def jogar():
     print("*********************")
     print("****Jogo de forca****")
     print("*********************")
 
-    palavraSecreta = "abacaxi"
-    letrasAcertadas = ["_", "_", "_", "_", "_", "_", "_"]
+    #implementar o uso da lista de carros
+    #carros = ["marea", "uno"]
 
+    #uso da lista na busca de frutas
+    frutas = ["maçã", "abacaxi", "uva", "laranja", "abacate", "tangerina", "figo", "acerola", "banana","romã", "manga", "mamão"]
+    palavraSecreta = random.choice(frutas).upper()
+
+    #para cada letra um _ no decorrer da palavra
+    letrasAcertadas = ["_" for letra in palavraSecreta]
+
+    #variaveis que param a aplicação se chegarem em true
     enforcado = False
     acertou = False
     tentativas = 0
 
+    print("Você só possui 3 tentativas: ")
+
     print(letrasAcertadas)
 
-    while (not enforcado & acertou):
-
+    #o laço onde o define se o player acertou ou perdeu
+    while (not enforcado and not acertou):
+        #entrada de dados do player
         chute = input("Qual letra você quer chutar? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        if(chute == palavraSecreta):
+        if(chute in palavraSecreta):
             index = 0
             for letra in palavraSecreta:
-                if (chute.upper() == letra.upper()):
+                if (chute == letra):
                     letrasAcertadas[index] = letra
-                index = index + 1
+                index += 1
         else:
-            tentativas = tentativas + 1
+            tentativas += 1
 
-        enforcado = tentativas == 2
+        enforcado = tentativas == 3
+        acertou = "_" not in letrasAcertadas
         print(letrasAcertadas)
-
+    if(acertou):
+        print("VOCÊ GANHOU CORNO(A)")
+    else:
+        print("VOCÊ É MUITO RUIM SEU OTARIO(A)")
+        #futuramente arrumar a frase para determinado tipo de categoria
+        print("A fruta era: ", palavraSecreta)
+    print("Fim de jogo")
 
 if(__name__ == "__main__"):
     jogar()
